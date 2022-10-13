c38399daaccb4a9f01cb1e9ca1bc534cbaef0a2f
JulianRDev <rosajulian1930@gmail.com>
2022-10-13 12:14:43 -0400

    ASCII art class updated, AB#255

--- src/main/java/com/bdj/dash/Main.java
+++ src/main/java/com/bdj/dash/Main.java
@@ -1,5 +1,14 @@
 package com.bdj.dash;
 
+import com.bdj.dash.controller.Game;
+
 public class Main {
+
+
+  public static void main(String[] args) {
+    Game newGame = new Game();
+
+    newGame.beginGame();
+  }
 
 }
