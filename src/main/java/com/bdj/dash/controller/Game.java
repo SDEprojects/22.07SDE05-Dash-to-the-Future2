34e720d219f17369d008f7b3d6720a569de8737b
JulianRDev <rosajulian1930@gmail.com>
2022-10-13 17:32:27 -0400

    add quit command AB#258

--- src/main/java/com/bdj/dash/controller/Game.java
+++ src/main/java/com/bdj/dash/controller/Game.java
@@ -1,16 +1,30 @@
 package com.bdj.dash.controller;
 
 import com.bdj.dash.model.Commands;
+import com.bdj.dash.model.Locations;
+import com.bdj.dash.model.Player;
 import com.bdj.dash.view.AsciiArt;
+import java.sql.SQLOutput;
 
 public class Game {
   AsciiArt titleArt = new AsciiArt();
   Commands commands = new Commands();
+  Locations location = new Locations();
+  Player player = new Player();
 
   public void beginGame(){
     titleArt.title();
     titleArt.instructions();
     commands.playGame();
+    while(commands.playing!=false) {
+      String userName = player.playerName();
+      if (userName.equals("quit")){
+        System.out.println("I guess you didn't want to play.");
+        break;
+      }
+      int pHp = player.getHealth();
+      System.out.println(userName + " your current health is = " + pHp);
+    }
   }
 
 }
--- src/main/java/com/bdj/dash/model/Player.java
+++ src/main/java/com/bdj/dash/model/Player.java
@@ -1,5 +1,32 @@
 package com.bdj.dash.model;
 
+import java.util.Scanner;
+
 public class Player {
 
+  int health = 100;
+  String name;
+
+  public String playerName() {
+    Scanner userName = new Scanner(System.in);
+    System.out.println("What is your name? ");
+    name = userName.nextLine().toLowerCase().trim();
+    return name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
+  }
 }
